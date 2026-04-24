9ace7c84f13028f0666b3c891dab7192f32f6a3d
Polina <pchdmh@gmail.com>
2026-04-24 12:28:04 +0300

    Добавление тестов с неправильной почтой

--- test_mail.py
+++ test_mail.py
@@ -15,7 +15,24 @@ class TestEmail(unittest.TestCase):
             with self.subTest(email=email):
                 self.assertTrue(is_valid_email(email))
 
-    
+    def test_incorrect_emails(self):
+        incorrect_list = [
+            "",
+            "address",
+            "@username.com",
+            "oksana@.com",
+            "polina@mail.",
+            "user@mail,ru",
+            "user@mail!.ru",
+            "user name@gmail.com",
+            "...@mail.ru",
+            "a@gmail.com",
+            "user@mail",
+            "p1@"
+        ]
+        for email in incorrect_list:
+            with self.subTest(email=email):
+                self.assertFalse(is_valid_email(email))
 
 if __name__ == '__main__':
     unittest.main()
